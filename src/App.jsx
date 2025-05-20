024ea6293e8ccc7ac0afdca76b574a52e6a36226
Brandon Segura Vazquez <bsegurav2100@alumno.ipn.mx>
2025-05-20 11:57:43 -0600

    nuevas paginas para modificar

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
+import Chat from './pages/Chat';
+import Solicitudes from './pages/Solicitudes';
 
 
 function App() {
@@ -10,6 +12,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/chat" element={<Chat />} />
+        <Route path="/solicitudes" element={<Solicitudes />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/Chat.jsx
@@ -0,0 +1,75 @@
+import { Link as RouterLink } from 'react-router-dom';
+import { Box, Typography, Button, Paper, Link } from '@mui/material';
+import { useTheme } from '@mui/material/styles';
+import React from 'react';
+
+export default function Home() {
+  const theme = useTheme();
+
+  return (
+    <Box
+      sx={{
+        minHeight: '100vh',
+        bgcolor: theme.palette.background.default,
+        color: theme.palette.primary.main,
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        p: { xs: 2, sm: 4 },
+      }}
+    >
+      <Paper
+        elevation={4}
+        sx={{
+          p: { xs: 2, sm: 4 },
+          bgcolor: theme.palette.background.paper,
+          borderRadius: theme.shape.borderRadius,
+          boxShadow: theme.shadows[3],
+          textAlign: 'center',
+        }}
+      >
+        <Typography
+          sx={{
+            ...theme.typography.bodyLarge,
+            color: theme.palette.primary.main,
+            fontWeight: 'bold',
+            mb: 2,
+          }}
+        >
+          Chat
+        </Typography>
+        <Typography
+          sx={{
+            ...theme.typography.bodySmall,
+            color: theme.palette.secondary.main,
+            mb: 3,
+            fontWeight: 'bold',
+          }}
+        >
+          Tu plataforma para encontrar y ofrecer servicios fácilmente xd.
+        </Typography>
+        <Button
+          variant="contained"
+          component={RouterLink}
+          to="/login"
+          sx={{
+            borderRadius: theme.shape.borderRadius,
+            px: theme.spacing(4),
+            py: theme.spacing(1.5),
+            ...theme.typography.bodySmall,
+            color: theme.palette.tertiary.main,
+            bgcolor: theme.palette.primary.main,
+            fontWeight: 'bold',
+            '&:hover': {
+              bgcolor: theme.palette.secondary.main,
+            },
+          }}
+        >
+          Iniciar Sesión
+        </Button>
+
+      </Paper>
+    </Box>
+  );
+}
