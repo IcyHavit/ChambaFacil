1d60ce2fe5af591b54aabd52c35e9cc27718750e
dngxue <paolareyes802@gmail.com>
2025-06-27 05:32:29 -0600

    pequeña mejora

--- src/pages/Publicaciones.jsx
+++ src/pages/Publicaciones.jsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import { Box, Typography } from '@mui/material';
+import { Box, Typography, Stack } from '@mui/material';
 import { useTheme } from '@mui/material/styles';
 import { useNavigate } from 'react-router-dom';
 // Importa ButtonMod
@@ -36,42 +35,46 @@ export default function Publicaciones() {
         Mis Publicaciones
       </Typography>
 
-      <Box
-        sx={{
-          width: '90vw',
-          height: '75vh',
-          position: 'relative', 
-          bgcolor: theme.palette.background.paper,
-          p: 2,
-          borderRadius: 2,
-          boxShadow: 1,
-          mt: 2,
-          overflow: 'auto',
-        }}
-      >
-        <Box
+      
+        <Stack
+        direction='column'
           sx={{
-            position: 'absolute',
+            width: '100%',
+            padding: '1%',
             top: theme.spacing(2),
             right: theme.spacing(2),
           }}
         >
-          <ButtonMod
-            variant="principal"
-            textCont="Publicar servicios"
-            width="auto"
-            height="auto"
-            clickEvent={() => navigate('/publicar')}
-          />
-        </Box>
+          <Box sx={{ display: 'flex', justifyContent: 'center', alignContent: 'center' }}>
+            <ButtonMod
+              variant="principal"
+              textCont="Publicar servicios"
+              width="auto"
+              height="auto"
+              clickEvent={() => navigate('/publicar')}
+            />
+          </Box>
 
-        {/* Cambia Typography por un contenedor flex para las cards */}
+          <Box
+            sx={{
+              width: '90vw',
+              height: '75vh',
+              position: 'relative', 
+              bgcolor: theme.palette.background.paper,
+              p: 2,
+              borderRadius: 2,
+              boxShadow: 1,
+              mt: 2,
+              overflow: 'auto',
+            }}
+          >
+            
         <Box
           sx={{
             display: 'flex',
             flexWrap: 'wrap',
-            gap: 3,
-            justifyContent: 'center', // Cambiado a center para centrar las cards
+            gap: 2,
+            justifyContent: 'center',
             alignItems: 'center',
             mt: 6,
           }}
@@ -90,7 +93,8 @@ export default function Publicaciones() {
             />
           ))}
         </Box>
+        </Box>
+        </Stack>
       </Box>
-    </Box>
   );
 }
